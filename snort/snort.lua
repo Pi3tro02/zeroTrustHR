284785e0dc276f09c2e7fbb2353dec2e68f6c393
Sebastianelli-Nicola <s1092763@studenti.univpm.it>
2026-06-26 12:38:51 +0200

    fix

--- snort/snort.lua
+++ snort/snort.lua
@@ -4,7 +4,8 @@ EXTERNAL_NET = 'any'
 ips = {
   rules = [[
     alert icmp any any -> any any (msg:"ICMP test detected"; sid:1000001; rev:1;)
-    alert tcp any any -> any 10000 (msg:"Nmap Scan Detected (Port 10000)"; flags:S; sid:1000002; rev:1;)
+    alert tcp any any -> any 10000 (msg:"Nmap Scan Detected (Port 10000)"; flags:S; detection_filter:track by_src, count 20, seconds 5; sid:1000002; rev:2;)
+    alert tcp any any -> any 10000 (msg:"HTTP Brute Force Detected"; content:"POST"; content:"/api/auth/login"; detection_filter:track by_src, count 10, seconds 5; sid:1000003; rev:1;)
   ]]
 }
 
